5ea7d1548d995c939d68580087fd2543b0567b26
uncool-and-uncouth <uncoolanduncouth@gmail.com>
2019-05-27 20:33:51 -0400

    Sprite now is animated in response to keyboard input.

--- src/gamescene.js
+++ src/gamescene.js
@@ -12,5 +12,44 @@ export default class GameScene extends Scene {
      }
     create() {
         this.comp = this.add.sprite(300, this.game.config.height/2, 'comp')
+        this.comp.setInteractive()
+        this.createWalkCycle()
+        this.createIdle()
+        this.cursors = this.input.keyboard.createCursorKeys();
+    }
+
+    update() {
+        if (this.cursors.right.isDown)
+        {
+            this.walk()
+        }
+        else {
+            this.comp.play('turn')
+        }
+    }
+
+    createWalkCycle() {
+        this.anims.create({
+            key: 'walk',
+            frames: [
+                { key: 'comp', frame: 1 },
+                { key: 'comp', frame: 2 },
+                { key: 'comp', frame: 3 }
+            ],
+            frameRate: 8,
+            repeat: -1
+        });
+    }
+
+    createIdle() {
+        this.anims.create({
+            key: 'turn',
+            frames: [ { key: 'comp', frame: 0 } ],
+            frameRate: 20
+        });
+    }
+
+    walk() {
+        this.comp.play('walk', true);
     }
 }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import Phaser from "phaser";
-import logoImg from "./assets/logo.png";
 import GameScene from './gamescene'
 
 const config = {
@@ -11,25 +10,3 @@ const config = {
 };
 
 const game = new Phaser.Game(config);
-
-
-
-//game.scene.add('gamescene', GameScene)
-
-// function preload() {
-//   this.load.image("logo", logoImg);
-//   this.load.spritesheet('comp', 'assets/comp_sheet-lg.png', { frameWidth: 128, frameHeight: 128 })
-// }
-
-// function create() {
-//   const logo = this.add.image(400, 150, "logo");
-
-//   this.tweens.add({
-//     targets: logo,
-//     y: 450,
-//     duration: 2000,
-//     ease: "Power2",
-//     yoyo: true,
-//     loop: -1
-//   });
-// }
